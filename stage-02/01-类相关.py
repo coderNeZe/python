8aef6fab6b5be3b652de7f3f3a985b51d0d2454c
wfh <shouwangwolf@163.com>
2018-04-13 13:43:38 +0800

    提交 类相关

--- stage-02/01-类相关.py
+++ stage-02/01-类相关.py
@@ -91,13 +91,40 @@ del Money2.age
 #print(Money2.age)  #报错
 
 #比如我想通过对象去删除一个类属性
-o = Money2()
-del o.number
+# o = Money2()
+# del o.number
 # print(o.number)  #会报错, 因为o自身没有number属性,只有类有number,所以删除不了类的属性
 
-#总结: 类型的查改删  只有查能通过对象去访问类属性,改和查都不能对类属性有任何操作
+#总结: 类属性的查改删 只有'查'能通过对象去访问类属性,'改'和'删'都不能通过对象 对 类属性有任何操作
 
+#类属性会被各个对象所共享
+oneM = Money2()
+twoM = Money2()
+print("共享",oneM.count,twoM.count) #两个打印的结果一致,说明类属性会被对象共享
 
+# 类属性和对象属性的一点小例子说明
+class Pet:
+    age = 10
 
+p = Pet()
+p.age += 5 #一开始p没有age属性,调用类的age 然后加上5  然后为p增加一个属性
 
+print(Pet.age)
+print(p.age)
+
+#如何限制对象去添加属性
+#python中,对象是可以随意的为类添加属性的,这就导致同一个类产生的不同对象,会有许多自己的东西,会导致非常的乱
+#此时,类中可以做限制,去限制对象添加属性
+class Student:
+    __slots__ = ["age"] #该类创建的对象,只能添加age属性
+    pass
+
+s1 = Student()
+s1.age = 1
+# s1.num = 2 #报错,只能添加age属性
+# print(s1.num)
+
+s2 = Student()
+# s2.name = "fh"  #报错,只能添加age属性
+# print(s2.name)
 
