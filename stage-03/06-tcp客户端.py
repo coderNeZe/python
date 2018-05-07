c8600c0463e1e3db6cb94a928ffe71cac7722302
wfh <shouwangwolf@163.com>
2018-05-07 22:57:22 +0800

    单进程非阻塞

--- stage-03/06-tcp客户端.py
+++ stage-03/06-tcp客户端.py
@@ -2,7 +2,7 @@ from socket import *
 
 clientSocket = socket(AF_INET,SOCK_STREAM)
 
-clientSocket.connect(("10.9.0.177", 8888))
+clientSocket.connect(("192.168.2.201", 8888))
 
 clientSocket.send("hahah".encode("utf-8"))
 recvData = clientSocket.recv(1024)
