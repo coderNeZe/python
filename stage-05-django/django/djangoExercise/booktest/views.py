c247c94cf43d918f705a5d9af379492093fdfac9
wfh <shouwangwolf@163.com>
2018-05-27 00:12:45 +0800

    django入门

--- stage-05-django/django/djangoExercise/booktest/views.py
+++ stage-05-django/django/djangoExercise/booktest/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import *
+# from django.template import RequestContext,loader
+from .models import *
 
-# Create your views here.
+def index(request):
+    # temp = loader.get_template('index.html')
+    # return HttpResponse(temp.render())
+    booklist = BookInfo.objects.all()
+    print(booklist)
+    context = {'list':booklist}
+    return render(request,'index.html',context)
+
+def show(request,id):
+    book = BookInfo.objects.get(pk=id)
+    herolist = book.heroinfo_set.all()
+    print(herolist)
+    context={'lsit':herolist}
+    return render(request,'show.html',context)
--- stage-05-django/django/djangoExercise/djangoExercise/settings.py
+++ stage-05-django/django/djangoExercise/djangoExercise/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'djangoExercise.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates/booktest')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'#'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'#'UTC'
 
 USE_I18N = True
 
